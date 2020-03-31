e3a97405b4b97bc36e0f9684a6fff102503c8f3c
alexlee-dev <alex@alexlee.dev>
2020-03-30 21:42:13 -0700

    🖍️ Sort By Hue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/extensions */
+import chalk from 'chalk';
 import ColorThief from 'colorthief';
 import path from 'path';
 // eslint-disable-next-line no-unused-vars
@@ -41,6 +42,57 @@ const rgb2Hsl = (colors: RGBColors): HSLColors => {
   return [h * 360, s * 100, l * 100];
 };
 
+const hsl2rgb = (colors: HSLColors): RGBColors => {
+  const h = colors[0];
+  let s = colors[1];
+  let l = colors[2];
+  // Must be fractions of 1
+  s /= 100;
+  l /= 100;
+
+  const c = (1 - Math.abs(2 * l - 1)) * s;
+  const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+  const m = l - c / 2;
+  let r = 0;
+  let g = 0;
+  let b = 0;
+
+  if (h >= 0 && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (h >= 60 && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (h >= 120 && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (h >= 180 && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (h >= 240 && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (h >= 300 && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+
+  return [r, g, b];
+};
+
+const sortByHue = (colors: HSLColors[]): HSLColors[] =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  colors.sort((a: HSLColors, b: HSLColors) => a[0] - b[0]);
+
 const main = async (): Promise<void> => {
   const img0 = path.join(__dirname, '../data/0.png');
   const img1 = path.join(__dirname, '../data/1.png');
@@ -75,6 +127,24 @@ const main = async (): Promise<void> => {
       hsl2,
       hsl3,
       hsl4,
+    });
+
+    const sorted = sortByHue([hsl0, hsl1, hsl2, hsl3, hsl4]);
+
+    console.log({ sorted });
+
+    const coolColors = sorted.map((colors: HSLColors) => hsl2rgb(colors));
+
+    coolColors.forEach((color: RGBColors) => {
+      console.log(
+        chalk.rgb(
+          color[0],
+          color[1],
+          // eslint-disable-next-line comma-dangle
+          color[2]
+          // eslint-disable-next-line comma-dangle
+        )('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+      );
     });
   } catch (e) {
     console.log(e);
