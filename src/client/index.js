0e83538e74adfd1bfc5d0303ee70b6bf9550bc15
alexlee-dev <alex@alexlee.dev>
2020-04-01 16:11:54 -0700

    ✨ Modify UI to Make Requests to Backend for Status on Process

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
   const [file, setFile] = useState(null);
   const [sorted, setSorted] = useState(null);
+  const [processId, setProcessId] = useState('');
 
   const handleInputChange = (e) => {
     const { files } = e.target;
@@ -18,9 +19,37 @@ const App = () => {
     data.append('file', file);
 
     fetch('/upload', { method: 'POST', body: data })
-      .then((response) => response.json())
-      .then((res) => setSorted(res));
+      .then((response) => {
+        if (response.status === 200) {
+          response
+            .json()
+            .then((res) => setProcessId(res.processId))
+            .catch((e) => console.error(e));
+        }
+      })
+      .catch((e) => console.error(e));
   };
+
+  useEffect(() => {
+    if (processId) {
+      const statusInterval = setInterval(() => {
+        fetch(`/status?id=${processId}`)
+          .then((response) => {
+            if (response.status === 200) {
+              response
+                .json()
+                .then((res) => {
+                  setSorted(res);
+                  setProcessId('');
+                  clearInterval(statusInterval);
+                })
+                .catch((e) => console.error(e));
+            }
+          })
+          .catch((e) => console.error(e));
+      }, 10000);
+    }
+  }, [processId]);
 
   return (
     <div>
@@ -30,6 +59,7 @@ const App = () => {
       <button onClick={handleUpload} type="button">
         UPLOAD
       </button>
+      {processId && <span>LOADING</span>}
       {sorted && (
         <div>
           {sorted.map((name, i) => (
