6df6e0b11cfc61bd69fc4b75d20e539fabf0ccab
alexlee-dev <alex@alexlee.dev>
2020-04-06 23:12:58 -0700

    ✏️ Use port from process always

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,7 +13,7 @@ const app = new App(
     new StatusController(currentProcesses),
     new UploadController(currentProcesses),
   ],
-  process.env.PORT || 3000,
+  process.env.PORT,
 );
 
 app.listen();
