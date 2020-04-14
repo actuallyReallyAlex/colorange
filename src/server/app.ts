92ac377fc693f8ae1ebc3388929732d0e086702d
alexlee-dev <alex@alexlee.dev>
2020-04-14 15:11:49 -0700

    ✏️ Modification for Travis 2

--- src/server/app.ts
+++ src/server/app.ts
@@ -34,6 +34,7 @@ class App {
   private initializeMiddlewares(): void {
     console.log(process.env.MONGODB_URL);
     console.log(typeof process.env.MONGODB_URL);
+    console.log(process.env);
     mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useCreateIndex: true,
