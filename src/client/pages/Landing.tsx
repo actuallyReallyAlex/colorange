00b9d360b05777c214a26776dfafc40ef6cccd6a
alexlee-dev <alex@alexlee.dev>
2020-04-17 11:42:03 -0700

    ✏️ Further modify Hero

--- src/client/pages/Landing.tsx
+++ src/client/pages/Landing.tsx
@@ -17,7 +17,7 @@ const useStyles = makeStyles(() => ({
   heroImage: {
     height: '500px',
     position: 'absolute',
-    right: '100px',
+    right: '0',
     zIndex: 1,
   },
   heroImageContainer: {
@@ -33,7 +33,10 @@ const useStyles = makeStyles(() => ({
     width: '1200px',
   },
   heroTypographyContainer: {
+    background: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: '50px',
     color: 'white',
+    padding: '25px',
     width: '67%',
     zIndex: 2,
   },
@@ -48,18 +51,35 @@ const useStyles = makeStyles(() => ({
 const Hero = () => {
   const classes = useStyles();
 
+  const addMarginToImage = () => {
+    const container = document.getElementById('hero-inner-container');
+    const image = document.getElementById('hero-image');
+    const marginLeft = window.getComputedStyle(container).marginLeft;
+
+    image.style.marginRight = marginLeft;
+  };
+
+  React.useEffect(() => {
+    addMarginToImage();
+
+    window.addEventListener('resize', addMarginToImage);
+  }, []);
+
   return (
     <Box className={classes.heroContainer} id="hero-container">
-      <Box className={classes.heroInnerContainer}>
+      <Box className={classes.heroInnerContainer} id="hero-inner-container">
         <Box className={classes.heroTypographyContainer}>
           <Typography className={classes.heroHeading} variant="h1">
-            Your phone is unorganized. Fix it.
+            Your phone is unorganized.
+          </Typography>
+          <Typography className={classes.heroHeading} variant="h1">
+            Let's fix it.
           </Typography>
           <Typography className={classes.heroSubheading} variant="h2">
             Colorange will make it better.
           </Typography>
         </Box>
-        <img className={classes.heroImage} src={HeroImage} />
+        <img className={classes.heroImage} id="hero-image" src={HeroImage} />
       </Box>
     </Box>
   );
